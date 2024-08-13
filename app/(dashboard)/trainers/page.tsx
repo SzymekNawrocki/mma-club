39c5bbe7b25f91bd734a8121814d4c5e211f1ef3
SzymekNawrocki <129281704+SzymekNawrocki@users.noreply.github.com>
2024-08-13 14:09:35 +0200

    vercel issue

--- app/(dashboard)/trainers/page.tsx
+++ app/(dashboard)/trainers/page.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Image from 'next/image';
 import { Card, CardHeader } from '@/components/ui/card';
 import { PageTitle } from "@/components/PageTitle";
+
 interface Trainer {
   title: string;
   description: string;
@@ -11,53 +12,44 @@ interface Trainer {
 }
 
 async function getTrainers() {
-  try {
-    const response = await fetch(process.env.NEXT_HYGRAPH_ENDPOINT as string, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        query: `query Trainers {
-                  trainers {
-                    title 
-                    description
-                    image {
-                      url
-                    }
+  const response = await fetch(process.env.NEXT_HYGRAPH_ENDPOINT as string, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: `query Trainers {
+                trainers {
+                  title 
+                  description
+                  image {
+                    url
                   }
-                }`,
-      }),
-    });
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    const json = await response.json();
-    return json.data.trainers;
-  } catch (error) {
-    console.error('Failed to fetch trainers:', error);
-    return [];
-  }
+                }
+              }`,
+    }),
+  });
+  const json = await response.json();
+  return json.data.trainers;
 }
 
-export default async function Trainers() {
-  const trainers: Trainer[] = await getTrainers();
-  console.log('Fetched trainers:', trainers);
+export default async function Fighters() {
+  const fighters: Trainer[] = await getTrainers();
 
   return (
     <div className="mt-12 py-12 flex flex-col items-center">
-      <PageTitle>Trenerzy</PageTitle>
-      {trainers.map((trainer: Trainer, index: number) => (
-        <div key={index} className="w-full max-w-md mt-4">
-          <Card className="shadow-lg rounded-lg">
+      <PageTitle>Zawodnicy</PageTitle>
+      {fighters.map((fighter: Trainer, index: number) => (
+        <div key={index} className="w-full max-w-md mb-8">
+          <Card className="shadow-lg rounded-lg mt-4">
             <CardHeader className="p-6">
-              <h1 className="text-2xl font-semibold mb-4 text-center">{trainer.title}</h1>
-              <p className="text-gray-600 mb-4 text-center">{trainer.description}</p>
+              <h1 className="text-2xl font-semibold mb-4 text-center">{fighter.title}</h1>
+              <p className="text-gray-600 mb-4 text-center">{fighter.description}</p>
               <div className="flex justify-center">
                 <div className="w-72 h-72 relative">
                   <Image
-                    src={trainer.image?.url}
-                    alt={trainer.title}
+                    src={fighter.image?.url}
+                    alt={fighter.title}
                     fill
                     className="object-cover rounded-lg"
                   />
